c113647e6cf383f5239e51da05c8af2de3c20570
suan-vinicius <suanv8@gmail.com>
2022-02-02 20:16:43 -0300

    environment variables

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,6 @@
+require('dotenv').config();
+
 export const config = {
     API_URL: 'http://localhost:5500',
-    MONGO_CONNECTION: 'mongodb+srv://developer:software@ecommerce.s2hj1.mongodb.net/shortURL?retryWrites=true&w=majority'
+    MONGO_CONNECTION: process.env.MONGO_URI
 }
